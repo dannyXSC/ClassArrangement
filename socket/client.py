2c3469e2edb7cc8daf110b391ba14135c4bbf7a6
dannyXSC <874794016@qq.com>
2020-12-24 11:56:35 +0800

    simple

---
+++ socket/client.py
@@ -0,0 +1,13 @@
+import socket
+
+
+class Client():
+    def __init__(self):
+        self.sleepTime = 0.1
+        self.targetPath = "../file"
+        self.serverIp = "127.0.0.1"
+        #self.serverIp="192.168.1.122"
+        self.serverPort = 8888
+
+
+print(1)
